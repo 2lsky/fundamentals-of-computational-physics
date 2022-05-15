3c04b42c61ac3f847121fca6e1b3fb0962c87d8f
Thomas Angelo <VitaminchikDima@gmail.com>
2022-05-15 18:00:50 +0700

    Исправления для 1 и 4 задания

--- venv/1ex.py
+++ venv/1ex.py
@@ -1,13 +1,14 @@
 import numpy as np
-def bubble_sort(nums):
+def bubble_sort(nums,order):
     swapped = True
     while swapped:
         swapped = False
         for i in range(len(nums) - 1):
-            if nums[i] > nums[i + 1]:
+            if nums[i] - nums[i + 1]>0:
+                order[i], order[i + 1] = order[i + 1], order[i]
                 nums[i], nums[i + 1] = nums[i + 1], nums[i]
                 swapped = True
-    return nums
+    return nums,order
 def define_eps32():
     j=1
     i=1
@@ -20,21 +21,26 @@ def define_eps32():
     for k in range(0,32-i):
         exp+=2**k
     eps=np.float32(1+1/(2**(i-1)))-np.float32(1)
-    mass=[np.float32(1),np.float32(1+eps/2),np.float32(1+eps),np.float32(1+eps+eps/2)]
-    return 'Mantissa discharge-{},UPL-{},max exp-{},min exp-{},comparison-{}'.format(i-1,eps,exp-int((exp+1)/2),-int((exp+1)/2),bubble_sort(mass))
+    mass={'1+3*e/2':np.float32(1+3*eps/2),'1+5*e/2':np.float32(1+5*eps/2),'1+e+e/2':np.float32(1+eps+eps/2),'1':np.float32(1)}
+    values=list(mass.values())
+    order=list(mass.keys())
+    return 'Mantissa discharge-{},UPL-{},max exp-{},min exp-{},comparison-{}'.format(i-1,eps,exp-int((exp+1)/2),-int((exp+1)/2),bubble_sort(values,order))
 def define_eps64():
     j = 1
     i = 1
     exp=0
     while (j > 0):
-        if (np.float64(1 + 1 / (2 ** i)) - np.float64(1)) == 0:
+        if (float(1 + 1 / (2 ** i)) - float(1)) == 0:
             break
         else:
             i += 1
     for k in range(0,64-i):
         exp+=2**k
-    eps=np.float64(1+1/(2**(i-1)))-np.float64(1)
-    mass = [np.float64(1), np.float64(1 + eps / 2), np.float64(1 + eps), np.float64(1 + eps + eps / 2)]
-    return 'Mantissa discharge-{},UPL-{},max exp-{},min exp-{},comparison-{}'.format(i-1,eps,exp-int((exp+1)/2),-int((exp+1)/2),bubble_sort(mass))
+    eps=float(1+1/(2**(i-1)))-float(1)
+    mass = {'1+3*e/2': np.float64(1+3*eps/2), '1+5*e/2': np.float64(1 + 5*eps/2), '1': np.float64(1),'1+e/2': np.float64(1+eps/2)}
+    values = list(mass.values())
+    order = list(mass.keys())
+    return 'Mantissa discharge-{},UPL-{},max exp-{},min exp-{},comparison-{}'.format(i-1,eps,exp-int((exp+1)/2),-int((exp+1)/2),bubble_sort(values,order))
 print(define_eps32())
 print(define_eps64())
+
--- venv/4ex.py
+++ venv/4ex.py
@@ -1,25 +1,37 @@
 import math
-import numpy as np
-def func(t,x,m):
-    return math.cos(m*t-x*math.sin(t))
-def Im(x,N):
-    h = math.pi / 2 ** N
-    interval = [0, math.pi]
-    I1=0
-    I_1=0
-    I__1=0
-    I2=0
-    I_2=0
-    I__2=0
-    t=0
-    for i in range(1,int((2**N)/2)):
-        I1+=2*func(t+2*i*h,x,1)
-        I_1+=2*func(t+2*i*h,x+h,0)
-        I__1+=2*func(t+2*i*h,x,0)
-    for i in range(1,int((2**N/2)+1)):
-        I2+=4*func(t+(2*i-1)*h,x,1)
-        I__2+= 4 * func(t+(2*i-1)*h, x, 0)
-        I_2+=4*func(t+(2*i-1)*h,x+h,0)
-    return ((I1+I2+func(interval[0],x,1)+func(interval[1],x,1))*h+(I_1+I_2+func(interval[0],x+h,0)+func(interval[1],x+h,0)-I__1-I__2-func(interval[0],x,0)-func(interval[0],x,0)))/3
-print(Im(float(input()),int(input())))
+import matplotlib.pyplot as plt
+def dI0(t,x):
+    return -math.sin(t)*math.sin(math.sin(t)*(x))
+def I1(t,x):
+    return math.cos(t-x*math.sin(t))
+def value(x,N):
+    h=math.pi/10**N
+    S1=0
+    for i in range(1,int((10**N)/2)):
+        S1+=2*(I1(2*i*h,x)+dI0(2*i*h,x))+4*(I1((2 * i - 1) * h, x) + dI0((2 * i - 1) * h, x))
+    return (S1+4*(I1(((10**N) - 1) * h, x) + dI0(((10**N) - 1) * h, x))+I1(0,x)+I1(math.pi,x)+dI0(0,x)+dI0(math.pi,x))*h/3
+def comparison(N,func,interval):
+    bool=True
+    h=(interval[1]-interval[0])/10**N
+    x0=interval[0]
+    for i in range(0,int(10**N)+1):
+        if math.fabs(func(x0+i*h,N))>(10**(-10)):
+            bool = False
+            break
+    return bool
+def graph(func,interval,N):
+    h = (interval[1] - interval[0]) / 10 ** N
+    x=[interval[0]+h*i for i in range(0,10**N+1)]
+    y=[func(x,N) for x in x]
+    plt.subplots()
+    plt.grid(True)
+    plt.xlabel('x', color='black')
+    plt.ylabel('dI0(x)/dx+I1(x)', color='black')
+    xmin=float('{:.4f}'.format(interval[0]))
+    xmax=float('{:.4f}'.format(interval[1]))
+    plt.text((interval[1]-interval[0])/2,5*max(y),f'x: from {xmin} to {xmax}...')
+    plt.axis([interval[0], interval[1], min(y)*10, max(y)*10])
+    plt.plot(x, y,'black')
+    plt.show()
+graph(value,[0,2*math.pi],int(input()))
 
